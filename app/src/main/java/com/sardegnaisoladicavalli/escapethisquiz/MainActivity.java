464238ac1d19145db7a024c0007a9e51f71b91ce
Melsara <kaxhortz@gmail.com>
2018-01-08 22:17:19 +0100

    Adding intent to start button to launch quiz.

--- app/src/main/java/com/sardegnaisoladicavalli/escapethisquiz/MainActivity.java
+++ app/src/main/java/com/sardegnaisoladicavalli/escapethisquiz/MainActivity.java
@@ -1,13 +1,24 @@
 package com.sardegnaisoladicavalli.escapethisquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startQuiz (View view) {
+        Intent intent = new Intent(this, QuizCard.class);
+        startActivity(intent);
     }
 }
