557d693de678a29ec03b1332734f7f0dea2e60d9
Melsara <kaxhortz@gmail.com>
2018-01-08 22:10:55 +0100

    Preparing for adding a second activity (Quiz card).

---
+++ app/src/main/java/com/sardegnaisoladicavalli/escapethisquiz/QuizCard.java
@@ -0,0 +1,13 @@
+package com.sardegnaisoladicavalli.escapethisquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuizCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_card);
+    }
+}
