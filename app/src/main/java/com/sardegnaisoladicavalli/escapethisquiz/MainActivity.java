2686a395dfa6e09a0691155fce9f4504b1ce3319
Melsara <kaxhortz@gmail.com>
2018-01-22 23:00:19 +0100

    Adding image resources.

--- app/src/main/java/com/sardegnaisoladicavalli/escapethisquiz/MainActivity.java
+++ app/src/main/java/com/sardegnaisoladicavalli/escapethisquiz/MainActivity.java
@@ -1,9 +1,11 @@
 package com.sardegnaisoladicavalli.escapethisquiz;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -39,6 +41,12 @@ public class MainActivity extends AppCompatActivity {
     {
 
         /*super.onBackPressed();*/
+    }
+
+    public void onClick(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
 
